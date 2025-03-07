b8a057fbea5a885f29ffdfa316204ac840ae197e
Simuzeche Kaluwa <simuzechek@gmail.com>
2025-03-07 18:46:51 +0200

    Feat : Added Repository to DI Container

--- RepositoryPatternDemo/Program.cs
+++ RepositoryPatternDemo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryPatternDemo.Data;
+using RepositoryPatternDemo.Repositories;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
